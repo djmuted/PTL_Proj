92d5d930562d77a2a7f825c3b33826684be33db2
djmuted <jaero13@gmail.com>
2020-05-19 19:53:25 +0200

    Add docker image

---
+++ docker/signal/files/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apk add --no-cache git nginx
+cd /opt
+git clone https://github.com/djmuted/PTL_Proj
+cd PTL_Proj
+cd signal
+npm install
+cd ..
+cd client-logic
+npm install
+cd ..
+cd client
+yarn install
+yarn build
