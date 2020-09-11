4aee81e45eec57fefd5fa37feefeb338ef342a59
djmuted <jaero13@gmail.com>
2020-09-11 13:36:38 +0200

    Fix kurento external IP to IPv4

--- docker/media/files/install.sh
+++ docker/media/files/install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -i "s|;externalAddress=10.20.30.40|externalAddress="`curl icanhazip.com`"|g" /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+sed -i "s|;externalAddress=10.20.30.40|externalAddress="`curl ipv4.icanhazip.com`"|g" /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
