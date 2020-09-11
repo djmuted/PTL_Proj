a6cd490098f18f788688ca8095e6bc0ce082130b
djmuted <jaero13@gmail.com>
2020-09-11 14:38:38 +0200

    Revert "Fix docker-compose file"
    This reverts commit 8fab7036869a674ee694ad6ea8ef13e20b43538f.

--- docker/signal/files/run.sh
+++ docker/signal/files/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /opt/PTL_Proj/signal
 nginx -g 'pid /tmp/nginx.pid;'
-PORT=5000 REDIS_IP="ptl-db" KURENTO_WS="ws://ptl-media:8888/kurento" npm start
+PORT=5000 REDIS_IP="ptl-db" KURENTO_WS="ws://"`route -n | grep 'UG[ \t]' | awk '{print $2}'`":8888/kurento" npm start
